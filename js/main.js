3e1aa12a5da15118a2e32d9c71693a8bf6c7509d
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-19 15:39:42 +0200

    add the validation with key enter

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
-var Robot = ["phrase 1", "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
 
+var pseudo = prompt("Quel est ton nom ?");
+console.log(pseudo);
+var Robot = ["Bonjour" + pseudo, "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
 
+var total = 0;
 // var jq = $.noConflict();
 
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
+function typing () {
+  $("#chat").append($("<img></img>").attr("src","img/typing.gif"));
+  $("#chat img:last").addClass("ktyping");
+}
 
-$("#ecrire :submit").click(function(){
-  $("#chat").append("<p>" + $("#ecrire :text").val() + "</p>");
+function chatting(){
+  $("#chat").append("<p class='utilisateur'>" + $("#ecrire :text").val() + "</p>");
   $("#ecrire :text").val("")
-  $("#chat p:last").addClass("utilisateur");
+  // $("#chat p:last").addClass("utilisateur");
 
-  setTimeout(function () {
-    $("#chat").append($("<img></img>").attr("src","img/typing.gif"));
-    $("#chat img:last").addClass("ktyping");
-  },1000);
+  setTimeout(typing
+
+  ,1000);
   setTimeout(function () {
     $("#chat img:last").remove();
     $("#chat").append($("<p></p>").text(Robot[0]));
@@ -26,15 +32,28 @@ Robot.shift();
 if ($("#chat p:last").text()=="je dois partir") {
   $("#chat").append($("<img></img>").attr("src","img/aurevoir.gif"));
   $("#chat img:last").addClass("ktyping");
+
 }
 
 
   }, "3000");
+}
+
+
+
+
+$('#ecrire :text').keypress(function(e){
+    if( e.which == 13 ){
+        chatting();
+    }
 });
 
 
+$("#ecrire :submit").click(chatting);
 
 
 
-});
+
+
+// });
 // .addClass("utilisateur")
