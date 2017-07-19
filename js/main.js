ac03fbb8ef7956a19da3287ca570718160355c93
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-19 11:55:28 +0200

    main.js: add the gif au revoir

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var Robot = ["phrase 1", "phrase 2", "phrase 3", "phrase 4"];
+var Robot = ["phrase 1", "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
 
 
 // var jq = $.noConflict();
@@ -13,13 +13,24 @@ $("#ecrire :submit").click(function(){
   $("#chat").append("<p>" + $("#ecrire :text").val() + "</p>");
   $("#ecrire :text").val("")
   $("#chat p:last").addClass("utilisateur");
+
   setTimeout(function () {
+    $("#chat").append($("<img></img>").attr("src","img/typing.gif"));
+    $("#chat img:last").addClass("ktyping");
+  },1000);
+  setTimeout(function () {
+    $("#chat img:last").remove();
     $("#chat").append($("<p></p>").text(Robot[0]));
 $("#chat p:last").addClass("karolina");
 Robot.shift();
-  }, 1000);
-});
+if ($("#chat p:last").text()=="je dois partir") {
+  $("#chat").append($("<img></img>").attr("src","img/aurevoir.gif"));
+  $("#chat img:last").addClass("ktyping");
+}
 
+
+  }, "3000");
+});
 
 
 
