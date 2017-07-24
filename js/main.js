e979bf045c5f6987345f78c0d6600325a29718ee
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-24 10:48:25 +0200

    make the front of the home page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 
-var pseudo = prompt("Quel est ton nom ?");
-console.log(pseudo);
-var Robot = ["Bonjour" + pseudo, "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
+var pseudo="" ;
+var Robot = ["Bonjour " + pseudo, "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
+
+
+
+
 
 var total = 0;
-// var jq = $.noConflict();
 
-
-
-// $(document).ready(function(){
-
+$("#lancerJeu").click(function(){$("#presentation").hide();
+pseudo = $("#presentation :text").val();
+Robot = ["Bonjour " + pseudo, "phrase 2", "phrase 3", "phrase 4", "je dois partir"];
+$("#ecrire").show();
+$("#chat").show();})
 
 function typing () {
-  $("#chat").append($("<img></img>").attr("src","img/typing.gif"));
-  $("#chat img:last").addClass("ktyping");
+  $("#chat").append($("<img class='ktyping'></img>").attr("src","img/typing.gif"));
+  // $("#chat img:last").addClass("ktyping");
+}
+
+function RobotChat() {
+
+    $("#chat img:last").remove();
+    $("#chat").append($("<p class='karolina'></p>").text(Robot[0]));
+// $("#chat p:last").addClass("karolina");
+Robot.shift();
+if ($("#chat p:last").text()=="je dois partir") {
+  $("#chat").append($("<img class ='ktyping'></img>").attr("src","img/aurevoir.gif"));
+  // $("#chat img:last").addClass("ktyping");
+
+}
+
+
 }
 
 function chatting(){
@@ -21,22 +39,8 @@ function chatting(){
   $("#ecrire :text").val("")
   // $("#chat p:last").addClass("utilisateur");
 
-  setTimeout(typing
-
-  ,1000);
-  setTimeout(function () {
-    $("#chat img:last").remove();
-    $("#chat").append($("<p></p>").text(Robot[0]));
-$("#chat p:last").addClass("karolina");
-Robot.shift();
-if ($("#chat p:last").text()=="je dois partir") {
-  $("#chat").append($("<img></img>").attr("src","img/aurevoir.gif"));
-  $("#chat img:last").addClass("ktyping");
-
-}
-
-
-  }, "3000");
+  setTimeout(typing,1000);
+  setTimeout(RobotChat , 3000);
 }
 
 
